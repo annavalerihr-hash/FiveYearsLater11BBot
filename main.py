c363a1ab9c2624f1f85621f744f60a7c59554640
annavalerihr-hash <annavalerihr@gmail.com>
2026-06-16 14:09:04 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+import sqlite3
+from datetime import datetime
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+TOKEN = "ТУТ_БУДЕ_ТОКЕН"
+
+OPEN_DATE = datetime(2031, 6, 27)
+
+conn = sqlite3.connect("videos.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS videos(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+user_id INTEGER,
+file_id TEXT
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+user_id INTEGER PRIMARY KEY
+)
+""")
+
+conn.commit()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+
+    cursor.execute(
+        "INSERT OR IGNORE INTO users(user_id) VALUES(?)",
+        (user_id,)
+    )
+
+    conn.commit()
+
+    await update.message.reply_text(
+        "🎓 Ласкаво просимо до капсули часу 11-Б!\n\nНадішліть відео, яке відкриється 27.06.2031 ❤️"
+    )
+
+async def receive_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    file_id = update.message.video.file_id
+
+    cursor.execute(
+        "INSERT INTO videos(user_id, file_id) VALUES(?, ?)",
+        (user_id, file_id)
+    )
+
+    conn.commit()
+
+    await update.message.reply_text(
+        "✅ Відео збережено!"
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.VIDEO, receive_video))
+
+app.run_polling()
